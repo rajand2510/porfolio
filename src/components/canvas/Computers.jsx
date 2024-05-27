fe9c861f4183e9f15b5dec96d1858b945efca010
Rajan Dhariyaparmar <rajandalvadi2510@gmail.com>
2024-05-27 13:12:49 +0530

    initial

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -61,7 +61,7 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [18, 3, 5], fov: 25 }}
+      camera={{ position: [17, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
